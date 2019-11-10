156a8748a3d824fbd261733ca654dda2a61f0d62
useHTML <mne@sait-nado.ru>
2019-11-10 16:58:54 +0500

    fix path in urls

--- des/urls.py
+++ des/urls.py
@@ -2,9 +2,8 @@
 from django.urls import path
 from des import views
 
-
 urlpatterns = [
-    path(r'^send-test-email', views.send_test_email, name = 'des-test-email'),
+    path('send-test-email', views.send_test_email, name='des-test-email'),
 ]
 
 __all__ = ['urlpatterns']
