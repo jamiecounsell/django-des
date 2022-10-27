606486834cd20155d9deef7bde047c48029919db
Hosam Hamdy <80757319+hosamhamdy258@users.noreply.github.com>
2022-10-27 11:00:30 +0200

    updated deprecated [url , force_text] to [re_path,force_str] to work on django 4.0

--- des/models.py
+++ des/models.py
@@ -1,49 +1,52 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-from django.utils.translation import ugettext as _
+try:
+    from django.utils.translation import ugettext as _
+except ImportError:
+    from django.utils.translation import gettext as _
 from solo.models import SingletonModel
 from django.core.exceptions import ValidationError
 
 
 class DynamicEmailConfiguration(SingletonModel):
     host = models.CharField(
-        blank = True, null = True,
-        max_length = 256, verbose_name = _("Email Host"))
+        blank=True, null=True,
+        max_length=256, verbose_name=_("Email Host"))
 
     port = models.SmallIntegerField(
-        blank = True, null = True,
-        verbose_name = _("Email Port"))
+        blank=True, null=True,
+        verbose_name=_("Email Port"))
 
     from_email = models.CharField(
-        blank = True, null = True,
-        max_length = 256, verbose_name = _("Default From Email"))
+        blank=True, null=True,
+        max_length=256, verbose_name=_("Default From Email"))
 
     username = models.CharField(
-        blank = True, null = True,
-        max_length = 256, verbose_name = _("Email Authentication Username"))
+        blank=True, null=True,
+        max_length=256, verbose_name=_("Email Authentication Username"))
 
     password = models.CharField(
-        blank = True, null = True,
-        max_length = 256, verbose_name = _("Email Authentication Password"))
+        blank=True, null=True,
+        max_length=256, verbose_name=_("Email Authentication Password"))
 
     use_tls = models.BooleanField(
-        default = False, verbose_name = _("Use TLS"))
+        default=False, verbose_name=_("Use TLS"))
 
     use_ssl = models.BooleanField(
-        default = False, verbose_name = _("Use SSL"))
+        default=False, verbose_name=_("Use SSL"))
 
     fail_silently = models.BooleanField(
-        default = False, verbose_name = _("Fail Silently"))
+        default=False, verbose_name=_("Fail Silently"))
 
     timeout = models.SmallIntegerField(
-        blank = True, null = True,
-        verbose_name = _("Email Send Timeout (seconds)"))
+        blank=True, null=True,
+        verbose_name=_("Email Send Timeout (seconds)"))
 
     def clean(self):
         if self.use_ssl and self.use_tls:
             raise ValidationError(
                 _("\"Use TLS\" and \"Use SSL\" are mutually exclusive, "
-                "so only set one of those settings to True."))
+                  "so only set one of those settings to True."))
 
     def __str__(self):
         return _("Email Configuration")
